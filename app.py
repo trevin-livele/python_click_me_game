cf2678f58d3b0c65011ae72f3e1a939a99fe3b1a
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-02-17 22:22:23 +0300

    installed requirements

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-from flask_script import Manager, Server
-from app import app,db
-from flask_migrate import Migrate,MigrateCommand
-from app.models.model import User
-from app.config import DevConfig
-
-app.config.from_object(DevConfig)
-
-manager = Manager(app)
-migrate = Migrate(app,db)
-
-manager.add_command('db',MigrateCommand)
-manager.add_command('server',Server)
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db)
-
+from app import app
 
 if __name__ == "__main__":
-    manager.run()
+    app.run()
--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,11 @@ from flask import Flask
 from .config import DevConfig, Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
-
-# Profile/Account imports
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import FileStorage
+# from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-# Profile/Account import end
+
+
 
 
 app = Flask(__name__)
@@ -19,19 +17,17 @@ app.config['SECRET_KEY']= Config.SECRET_KEY
 app.config['UPLOADED_PHOTOS_DEST']= Config.UPLOADED_PHOTOS_DEST
 # End of Configs
 
-bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+Migrate(app,db)
 # Profile/account 
 photos = UploadSet('photos',IMAGES)
 # End of profile/account instantiating
 
-bootstrap.init_app(app)
+# bootstrap.init_app(app)
 db.init_app(app)
 
 # Profile/account 
 configure_uploads(app,photos)
-
 login_manager= LoginManager(app)
 login_manager.login_view='login'
 
